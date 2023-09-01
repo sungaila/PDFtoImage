89d2cf9d7cd3ac6da3bd827bd733ab57d7b3dfe8
David Sungaila <david.sungaila@arcor.de>
2023-09-01 14:51:43 +0200

    Current implementation

--- src/WebConverter/Client/Program.cs
+++ src/WebConverter/Client/Program.cs
@@ -1,23 +1,57 @@
+using KristofferStrube.Blazor.FileSystem;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Thinktecture.Blazor.FileHandling;
 
 namespace PDFtoImage.WebConverter
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+	public class Program
+	{
+		public static event EventHandler<HandledFileArgs>? FilesHandled;
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            await builder.Build().RunAsync();
-        }
-    }
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddFileHandlingService();
+
+			var host = builder.Build();
+			var navigationManager = host.Services.GetService<NavigationManager>()!;
+
+			if (host.Services.GetService<FileHandlingService>() is FileHandlingService service && await service.IsSupportedAsync())
+			{
+				await service.SetConsumerAsync(async (launchParams) =>
+				{
+					if (launchParams == null || !launchParams.Files.Any())
+						return;
+
+					if (launchParams.Files[0] is FileSystemFileHandle fileSystemFileHandle)
+					{
+						FilesHandled?.Invoke(null, new HandledFileArgs(await fileSystemFileHandle.GetFileAsync()));
+					}
+				});
+			}
+
+			await host.RunAsync();
+		}
+
+		public class HandledFileArgs : EventArgs
+		{
+			public KristofferStrube.Blazor.FileAPI.File File { get; }
+
+			public HandledFileArgs(KristofferStrube.Blazor.FileAPI.File file)
+			{
+				File = file;
+			}
+		}
+	}
 }
---
+++ src/WebConverter/Models/DummyFile.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace PDFtoImage.WebConverter.Models
+{
+	public record DummyFile(string Name, DateTimeOffset LastModified, long Size, string ContentType) : IBrowserFile
+	{
+		public Stream OpenReadStream(long maxAllowedSize = 512000, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+	}
+}
