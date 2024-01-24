c976e1384ea9f97d370a5ff08ab1efd6d9c275c7
David Sungaila <david.sungaila@arcor.de>
2024-01-24 14:30:19 +0100

    Fix Mono DllImport and add MonoConsole test

--- src/PDFtoImage/PdfiumViewer/NativeMethods.cs
+++ src/PDFtoImage/PdfiumViewer/NativeMethods.cs
@@ -2,13 +2,5 @@
 {
     internal static partial class NativeMethods
     {
-        static NativeMethods()
-        {
-#if NETFRAMEWORK
-            LibraryLoader.LoadLocalLibrary<PdfDocument>("pdfium");
-#else
-            // .NET (Core) and Xamarin resolve the pdfium lib on their own
-#endif
-        }
     }
 }
