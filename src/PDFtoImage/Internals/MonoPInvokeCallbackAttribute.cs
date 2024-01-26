c9bf4e050178f75bd6b677c865a605064cca43ee
David Sungaila <david.sungaila@arcor.de>
2024-01-26 22:17:20 +0100

    Add support for Unity

---
+++ src/PDFtoImage/Internals/MonoPInvokeCallbackAttribute.cs
@@ -0,0 +1,9 @@
+﻿#if !NET6_0_OR_GREATER
+using System;
+
+namespace Mono.Util
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    internal sealed class MonoPInvokeCallbackAttribute : Attribute { }
+}
+#endif
