857e0c70a1e63a123d0cbce907606bf7b2d9bd4c
David Sungaila <david.sungaila@arcor.de>
2024-07-24 18:15:39 +0200

    Add unit tests for new methods

--- src/Tests/BatchingTests.cs
+++ src/Tests/BatchingTests.cs
@@ -1,22 +1,202 @@
-using Microsoft.VisualBasic;
+#if NET6_0_OR_GREATER
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PDFtoImage.Tests;
+using SkiaSharp;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using static PDFtoImage.Conversion;
 using static PDFtoImage.Tests.TestUtils;
 
 namespace Tests
 {
-	[TestClass]
-	public class BatchingTests : TestBase
-	{
-		[TestMethod]
-        [DataRow("hundesteuer-anmeldung.pdf")]
-        public void WithoutAspectRatio(string fileName)
-		{
-			using var inputStream = GetInputStream(Path.Combine("..", "Assets", fileName));
+    [TestClass]
+    public class BatchingTests : TestBase
+    {
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(6)]
+        [DataRow(7)]
+        [DataRow(8)]
+        [DataRow(9)]
+        [DataRow(10)]
+        [DataRow(11)]
+        [DataRow(12)]
+        [DataRow(13)]
+        [DataRow(14)]
+        [DataRow(15)]
+        [DataRow(16)]
+        [DataRow(17)]
+        [DataRow(18)]
+        [DataRow(19)]
+        public void ToImageWithInteger(int? page)
+        {
+            var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{page ?? 0}.png");
 
-			var test = ToImages(inputStream);
-		}
-	}
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+            using var outputStream = CreateOutputStream(expectedPath);
+
+            if (page == null)
+                SavePng(outputStream, inputStream, options: new(Dpi: 40));
+            else
+                SavePng(outputStream, inputStream, page: page.Value, options: new(Dpi: 40));
+
+            CompareStreams(expectedPath, outputStream);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(6)]
+        [DataRow(7)]
+        [DataRow(8)]
+        [DataRow(9)]
+        [DataRow(10)]
+        [DataRow(11)]
+        [DataRow(12)]
+        [DataRow(13)]
+        [DataRow(14)]
+        [DataRow(15)]
+        [DataRow(16)]
+        [DataRow(17)]
+        [DataRow(18)]
+        [DataRow(19)]
+        public void ToImageWithIndex(int page)
+        {
+            var index = (Index)page;
+            var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{index.GetOffset(20)}.png");
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+            using var outputStream = CreateOutputStream(expectedPath);
+
+            SavePng(outputStream, inputStream, index, options: new(Dpi: 40));
+
+            CompareStreams(expectedPath, outputStream);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(6)]
+        [DataRow(7)]
+        [DataRow(8)]
+        [DataRow(9)]
+        [DataRow(10)]
+        [DataRow(11)]
+        [DataRow(12)]
+        [DataRow(13)]
+        [DataRow(14)]
+        [DataRow(15)]
+        [DataRow(16)]
+        [DataRow(17)]
+        [DataRow(18)]
+        [DataRow(19)]
+        public void ToImageWithIndexFromEnd(int page)
+        {
+            var index = new Index(page + 1, true);
+            var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{index.GetOffset(20)}.png");
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+            using var outputStream = CreateOutputStream(expectedPath);
+
+            SavePng(outputStream, inputStream, index, options: new(Dpi: 40));
+
+            CompareStreams(expectedPath, outputStream);
+        }
+
+        [TestMethod]
+        public async Task ToImagesWithRangeAll()
+        {
+            var range = 0..;
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+
+            int i = range.Start.Value;
+
+            await foreach (var bitmap in ToImagesAsync(inputStream, range, options: new(Dpi: 40)))
+            {
+                var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{i}.png");
+                using var outputStream = CreateOutputStream(expectedPath);
+                bitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+
+                CompareStreams(expectedPath, outputStream);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public async Task ToImagesWithRangeSecondHalf()
+        {
+            var range = 10..;
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+
+            int i = range.Start.Value;
+
+            await foreach (var bitmap in ToImagesAsync(inputStream, range, options: new(Dpi: 40)))
+            {
+                var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{i}.png");
+                using var outputStream = CreateOutputStream(expectedPath);
+                bitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+
+                CompareStreams(expectedPath, outputStream);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public async Task ToImagesWithSelectionEven()
+        {
+            int[] selection = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+
+            int i = 0;
+
+            await foreach (var bitmap in ToImagesAsync(inputStream, selection, options: new(Dpi: 40)))
+            {
+                var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{selection[i]}.png");
+                using var outputStream = CreateOutputStream(expectedPath);
+                bitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+
+                CompareStreams(expectedPath, outputStream);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public async Task ToImagesWithSelectionOdd()
+        {
+            int[] selection = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+            using var inputStream = GetInputStream(Path.Combine("..", "Assets", "Wikimedia_Commons_web.pdf"));
+
+            int i = 0;
+
+            await foreach (var bitmap in ToImagesAsync(inputStream, selection, options: new(Dpi: 40)))
+            {
+                var expectedPath = Path.Combine("..", "Assets", "Expected", GetPlatformAsString(), $"Wikimedia_Commons_web_{selection[i]}.png");
+                using var outputStream = CreateOutputStream(expectedPath);
+                bitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+
+                CompareStreams(expectedPath, outputStream);
+                i++;
+            }
+        }
+    }
 }
+#endif
