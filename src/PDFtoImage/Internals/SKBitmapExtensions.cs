0acb35e4124c38ae727023f8ffbc2271ffceddf3
David Sungaila <david.sungaila@arcor.de>
2024-10-18 12:35:23 +0200

    Update Obsolete message

--- src/PDFtoImage/Internals/SKBitmapExtensions.cs
+++ src/PDFtoImage/Internals/SKBitmapExtensions.cs
@@ -6,7 +6,7 @@ namespace PDFtoImage.Internals
 {
     internal static class SKBitmapExtensions
     {
-        [Obsolete("This is a workaround for the missing PNG compression in SkiaSharp 3.x. Remove this once SkiaSharp is fixed.")]
+        [Obsolete("This is a workaround for the missing PNG compression in SkiaSharp 3.x. Remove this once SkiaSharp is fixed. See GitHub issue: https://github.com/mono/SkiaSharp/issues/3013")]
         public static bool EncodeExt(this SKBitmap bitmap, Stream dst, SKEncodedImageFormat format, int quality)
         {
             if (format == SKEncodedImageFormat.Png)
