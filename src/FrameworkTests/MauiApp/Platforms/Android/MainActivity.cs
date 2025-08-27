88c87c207f65c5d108f95b1577baad154f548056
David Sungaila <david.sungaila@arcor.de>
2025-08-27 14:19:21 +0200

    Update MAUI test project

--- src/FrameworkTests/MauiApp/Platforms/Android/MainActivity.cs
+++ src/FrameworkTests/MauiApp/Platforms/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Content.PM;
 
 namespace PDFtoImage.FrameworkTests.MauiApp.Platforms.Android
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
--- src/FrameworkTests/MauiApp/Platforms/Android/MainApplication.cs
+++ src/FrameworkTests/MauiApp/Platforms/Android/MainApplication.cs
@@ -4,8 +4,14 @@ using Android.Runtime;
 namespace PDFtoImage.FrameworkTests.MauiApp.Platforms.Android
 {
     [Application]
-    public class MainApplication(nint handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+    public class MainApplication : MauiApplication
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0290")]
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
         protected override Microsoft.Maui.Hosting.MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
